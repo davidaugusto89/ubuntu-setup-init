74925f990b2b81c56f0ee412d6dd86073265fa6b
David Augusto <davidakhaddad@gmail.com>
2025-01-09 11:19:29 -0300

    Install Discord snap

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
 sudo apt install -y code
-sudo snap install insomnia -y
+sudo snap install insomnia discord
 sudo apt update
 sleep 1
 
